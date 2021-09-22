83b7f90b7ffa9fd1c8ae253bb02d72e892039002
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2021-09-22 04:09:04 +0200

    initial

---
+++ Application/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var version = typeof(Program).Assembly.GetName().Version;
+            Console.WriteLine($"Application {version}");
+        }
+    }
+}
---
+++ Updater/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = typeof(Program).Assembly.GetName().Version;
+            Console.WriteLine($"Updater {version}");
+        }
+    }
+}
