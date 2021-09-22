805d1ea46c9da87756eb510e6262dfde2b155647
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2021-09-22 04:49:54 +0200

    updater

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace Updater
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public const string TEMP_DIR = "temp";
+        private const string DIR_IN_ZIP = "Application";
+
+        public static void Main(string[] args)
         {
             var version = typeof(Program).Assembly.GetName().Version;
             Console.WriteLine($"Updater {version}");
+
+            var path = Path.Combine(TEMP_DIR, DIR_IN_ZIP);
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                var dstPath = dir[(path.Length + 1)..^0];
+                if (Directory.Exists(dstPath))
+                    Directory.Delete(dstPath, true);
+                Directory.Move(dir, dstPath);
+            }
+            foreach(var file in Directory.GetFiles(path))
+            {
+                var dstPath = file[(path.Length + 1)..^0];
+                File.Move(file, dstPath, true);
+            }
         }
     }
 }
